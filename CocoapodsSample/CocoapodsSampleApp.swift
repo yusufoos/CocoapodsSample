2f7d5824aa7fb30d0d182f5b7c62e622cbd6cd86
Yusuf Sobh <ysobh@linkedin.com>
2023-10-27 12:33:47 -0500

    Initial

---
+++ CocoapodsSample/CocoapodsSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocoapodsSampleApp.swift
+//  CocoapodsSample
+//
+//  Created by Yusuf Sobh on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct CocoapodsSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
